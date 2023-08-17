bad88407bd38fb3e8dc6c7226e4c47466bd59e1b
Philipp Schilk <schilk.philipp@gmail.com>
2023-08-17 16:59:37 +0200

    NVIM: Removed cmp-cmdline
    buggy/not working right. Not sure if actually benefitial.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -37,7 +37,6 @@ require('lazy').setup({
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-nvim-lsp-signature-help',
             'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip',
