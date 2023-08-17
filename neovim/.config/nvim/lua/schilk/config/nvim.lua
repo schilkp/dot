0c70063eaed9e36d28d04962c1842442b15cd9d2
Philipp Schilk <schilk.philipp@gmail.com>
2023-08-17 15:46:30 +0200

    NVIM: Removed floaterm, added tab+terminal crutch binding.

--- neovim/.config/nvim/lua/schilk/config/nvim.lua
+++ neovim/.config/nvim/lua/schilk/config/nvim.lua
@@ -11,4 +11,12 @@ function M.config_highlight_on_yank()
     })
 end
 
+function M.config_floatterm_replacement()
+    local function open_term_tab()
+        vim.cmd("tabnew")
+        vim.cmd("terminal")
+    end
+    vim.keymap.set({ 'n' }, '<leader>tt', open_term_tab, { silent = true, desc = "Open terminal tab." })
+end
+
 return M
