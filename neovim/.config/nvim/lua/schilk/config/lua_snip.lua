844015839709357cd08296c9af188eeed7c630fe
Philipp Schilk <schilk.philipp@gmail.com>
2023-08-25 17:58:16 +0200

    nvim: fix luasnip c++ filetype

--- neovim/.config/nvim/lua/schilk/config/lua_snip.lua
+++ neovim/.config/nvim/lua/schilk/config/lua_snip.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
     local luasnip = require('luasnip')
     luasnip.add_snippets('c', require('schilk.snippets.c').snippets);
-    luasnip.add_snippets('c++', require('schilk.snippets.c').snippets);
+    luasnip.add_snippets('cpp', require('schilk.snippets.c').snippets);
     luasnip.add_snippets('tex', require('schilk.snippets.tex').snippets);
     luasnip.add_snippets('python', require('schilk.snippets.python').snippets);
     luasnip.add_snippets('rust', require('schilk.snippets.rust').snippets);
