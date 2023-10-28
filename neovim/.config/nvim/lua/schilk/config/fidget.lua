021eefe4dc34db7157f3a6bdf03b4df039d899fd
Philipp Schilk <schilk.philipp@gmail.com>
2023-10-28 09:42:41 +0200

    nvim: removed ltex fidget message spam.

--- neovim/.config/nvim/lua/schilk/config/fidget.lua
+++ neovim/.config/nvim/lua/schilk/config/fidget.lua
@@ -3,7 +3,12 @@ local M = {}
 function M.config()
     require("fidget").setup({
         text = { spinner = "dots_snake" },
-        window = { blend = 0 }
+        window = { blend = 0 },
+        sources = {
+            ltex = {
+                ignore = true
+            }
+        }
     });
 end
 
