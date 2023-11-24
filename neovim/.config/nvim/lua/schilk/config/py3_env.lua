8f213bd807cc69b7912fe73214b3cac60c4f5a41
Philipp Schilk <schilk.philipp@gmail.com>
2023-11-24 16:25:38 +0100

    NVIM: Improved py3 install output a little bit.

--- neovim/.config/nvim/lua/schilk/config/py3_env.lua
+++ neovim/.config/nvim/lua/schilk/config/py3_env.lua
@@ -4,7 +4,7 @@ M.py_env_dir = vim.fs.normalize('~/.config/nvim/nvim_env')
 M.py = vim.fs.normalize(M.py_env_dir .. '/bin/python')
 
 function M.config_py3_env()
-    vim.cmd("command CreatePy3Env lua require('schilk.config.nvim').create_py3_env()")
+    vim.cmd("command CreatePy3Env lua require('schilk.config.py3_env').create_py3_env()")
     if (vim.fn.filereadable(M.py) == 1) then
         -- Venv exists: Use it
         vim.g.python3_host_prog = M.py
@@ -20,14 +20,14 @@ function M.create_py3_env()
         return
     end
 
-    local e = os.execute('python3 -m venv "' .. M.py_env_dir .. '" 1>/dev/null 2>/dev/null')
-    if (e ~= 0) then
+    vim.cmd('!python3 -m venv "' .. M.py_env_dir .. '"')
+    if (vim.v.shell_error ~= 0) then
         vim.print("Unabled to create.")
         return
     end
 
-    e = os.execute(M.py .. ' -m pip install pynvim 1>/dev/null 2>/dev/null')
-    if (e ~= 0) then
+    vim.cmd("!" .. M.py .. ' -m pip install pynvim 1>/dev/null')
+    if (vim.v.shell_error ~= 0) then
         vim.print("Unable to install pynvim in venv!")
         return
     end
