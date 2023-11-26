e3b90a8d09bdfe041922177628ff438ebdfde4b0
Philipp Schilk <schilk.philipp@gmail.com>
2023-11-26 11:47:19 +0100

    tmux: better session/layout management.

---
+++ utils/tmux_session.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sh
+
+selected=$(                                                                          \
+    find ~/es ~/patch-it ~/reps ~/dotfiles ~/.config -mindepth 1 -maxdepth 3 -type d \
+    | grep -v -F -e ".git" -e ".bfg-report" -e "__pycache__"                         \
+    | fzf                                                                            \
+)
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
