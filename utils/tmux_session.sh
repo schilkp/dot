879eccdba1f523268fa3c3c91f399367a74bc811
Philipp Schilk <schilk.philipp@gmail.com>
2024-01-01 14:07:03 +0100

    TMUX: Have new-session script show paths relative to home dir.

--- utils/tmux_session.sh
+++ utils/tmux_session.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/sh
 
-selected=$(                                                   \
-    {                                                         \
-      find ~/es ~/patch-it -mindepth 1 -maxdepth 3 -type d && \
-      find ~/reps          -mindepth 1 -maxdepth 1 -type d && \
-      find ~/dotfiles      -mindepth 0 -maxdepth 1 -type d;   \
-    }                                                         \
-    | grep -v -F -e ".git" -e ".bfg-report" -e "__pycache__"  \
-    | fzf                                                     \
+selected=$(                                                                                        \
+    {                                                                                              \
+      find ~/es ~/patch-it -mindepth 1 -maxdepth 3 -type d &&                                      \
+      find ~/reps          -mindepth 1 -maxdepth 1 -type d &&                                      \
+      find ~/dotfiles      -mindepth 0 -maxdepth 1 -type d;                                        \
+    }                                                                                              \
+    | grep -v -F -e ".git" -e ".bfg-report" -e "__pycache__" `# Remove pointless files.`           \
+    | xargs -L 1 realpath --relative-to="${HOME}"            `# Remove path to home directory.`    \
+    | fzf                                                    `# Picker.`                           \
+    | sed -E "sQ^Q${HOME}/Q"                                 `# Add path again`                    \
 )
 
-if [[ -z $selected ]]; then
+if [ -z "$selected" ]; then
     exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
+if [ -z "$TMUX" ] && [ -z "$tmux_running" ]; then
+    tmux new-session -s "$selected_name" -c "$selected"
     exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t $selected_name
+tmux switch-client -t "$selected_name"
