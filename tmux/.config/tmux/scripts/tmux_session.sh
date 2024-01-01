5c403470616f610be65c93b4f0e8b7005583e23e
Philipp Schilk <schilk.philipp@gmail.com>
2024-01-01 21:07:06 +0100

    TMUX: Don't filter out .github in session picker.
    I couldn't pick `schilkp.github.io` because it got caught in the `.git`
    filter.

--- tmux/.config/tmux/scripts/tmux_session.sh
+++ tmux/.config/tmux/scripts/tmux_session.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/sh
 
-selected=$(                                                                                        \
-    {                                                                                              \
-      find ~/es ~/patch-it -mindepth 1 -maxdepth 3 -type d &&                                      \
-      find ~/reps          -mindepth 1 -maxdepth 1 -type d &&                                      \
-      find ~/dotfiles      -mindepth 0 -maxdepth 1 -type d;                                        \
-    }                                                                                              \
-    | grep -v -F -e ".git" -e ".bfg-report" -e "__pycache__" `# Remove pointless files.`           \
-    | xargs -L 1 realpath --relative-to="${HOME}"            `# Remove path to home directory.`    \
-    | fzf                                                    `# Picker.`                           \
-    | sed -E "sQ^Q${HOME}/Q"                                 `# Add path again`                    \
+selected=$(                                                                                    \
+    {                                                                                          \
+      find ~/es ~/patch-it -mindepth 1 -maxdepth 3 -type d &&                                  \
+      find ~/reps          -mindepth 1 -maxdepth 1 -type d &&                                  \
+      find ~/dotfiles      -mindepth 0 -maxdepth 1 -type d;                                    \
+    }                                                                                          \
+    | grep -v -F -e ".bfg-report" -e "__pycache__"   `# Remove pointless files.`               \
+    | grep -v -e "\.git\($\|/\)"                     `# Remove .git and .git/ but not .github` \
+    | xargs -L 1 realpath --relative-to="${HOME}"    `# Remove path to home directory.`        \
+    | fzf                                            `# Picker.`                               \
+    | sed -E "sQ^Q${HOME}/Q"                         `# Add path again`                        \
 )
 
 if [ -z "$selected" ]; then
