e56b64a886a784d277fe64f24822e7858bb4d2aa
Philipp Schilk <schilk.philipp@gmail.com>
2024-02-04 16:26:12 +0100

    tmux: handle "main" in session script

--- tmux/.config/tmux/scripts/tmux_session.sh
+++ tmux/.config/tmux/scripts/tmux_session.sh
@@ -5,29 +5,37 @@ selected=$(                                                                     
       find ~/es ~/patch-it -mindepth 1 -maxdepth 3 -type d &&                                  \
       find ~/reps          -mindepth 1 -maxdepth 1 -type d &&                                  \
       find ~/dotfiles      -mindepth 0 -maxdepth 1 -type d &&                                  \
-      realpath ~/calc;                                                                         \
+      realpath ~/calc &&                                                                       \
+      echo "main";                                                                             \
     }                                                                                          \
     | grep -v -F -e ".bfg-report" -e "__pycache__"   `# Remove pointless files.`               \
     | grep -v -e "\.git\($\|/\)"                     `# Remove .git and .git/ but not .github` \
     | xargs -L 1 realpath --relative-to="${HOME}"    `# Remove path to home directory.`        \
-    | fzf                                            `# Picker.`                               \
-    | sed -E "sQ^Q${HOME}/Q"                         `# Add path again`                        \
+    | fzf --reverse                                  `# Picker.`
 )
 
+# Exit if nothing selected:
 if [ -z "$selected" ]; then
     exit 0
+fi
+
+# If "main" was selected, set path to home. Otherwise, set path to selected directory:
+if [ "$selected" = "main" ]; then
+    selected_path="${HOME}"
+else
+    selected_path=$(echo "${selected}" | sed -E "sQ^Q${HOME}/Q")
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [ -z "$TMUX" ] && [ -z "$tmux_running" ]; then
-    tmux new-session -s "$selected_name" -c "$selected"
+    tmux new-session -s "$selected_name" -c "$selected_path"
     exit 0
 fi
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
+    tmux new-session -ds "$selected_name" -c "$selected_path"
 fi
 
 tmux switch-client -t "$selected_name"
