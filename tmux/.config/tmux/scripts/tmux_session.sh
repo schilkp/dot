83b94d78e3a57187a3e98c4b582dd46410e15ff3
Philipp Schilk <schilk.philipp@gmail.com>
2024-02-05 10:44:57 +0100

    TMUX: Improved session script.

--- tmux/.config/tmux/scripts/tmux_session.sh
+++ tmux/.config/tmux/scripts/tmux_session.sh
@@ -1,23 +1,42 @@
-#!/usr/bin/sh
-
-selected=$(                                                                                    \
+#!/usr/bin/bash
+options_dirs=$(                                                                                \
     {                                                                                          \
       find ~/es ~/patch-it -mindepth 1 -maxdepth 3 -type d &&                                  \
       find ~/reps          -mindepth 1 -maxdepth 1 -type d &&                                  \
       find ~/dotfiles      -mindepth 0 -maxdepth 1 -type d &&                                  \
-      realpath ~/calc &&                                                                       \
-      echo "main";                                                                             \
+      realpath ~/calc;                                                                         \
     }                                                                                          \
     | grep -v -F -e ".bfg-report" -e "__pycache__"   `# Remove pointless files.`               \
     | grep -v -e "\.git\($\|/\)"                     `# Remove .git and .git/ but not .github` \
     | xargs -L 1 realpath --relative-to="${HOME}"    `# Remove path to home directory.`        \
-    | fzf --reverse                                  `# Picker.`
+)
+
+options_sessions=$(tmux list-sessions -F "#{session_name} [#{session_id}]")
+
+options_others="main"
+
+selected=$(                                                                                    \
+    {                                                                                          \
+        echo "${options_others}" &&                                                            \
+        echo "${options_sessions}" &&                                                          \
+        echo "${options_dirs}";                                                                \
+    }                                                                                          \
+    | fzf --reverse --scheme=path
 )
 
 # Exit if nothing selected:
 if [ -z "$selected" ]; then
     exit 0
 fi
+
+# If an existing session was selected, go to that session:
+if [[ $selected =~ \[\$[0-9]+\]$ ]];  then
+    selected_session=$(echo "$selected" | sed -E 's/ \[\$([0-9]+)\]$//')
+    tmux switch-client -t "$selected_session"
+    exit
+fi
+
+# Go to path-session.
 
 # If "main" was selected, set path to home. Otherwise, set path to selected directory:
 if [ "$selected" = "main" ]; then
