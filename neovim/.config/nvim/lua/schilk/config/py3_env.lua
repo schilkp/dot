912fe5a8741bef865ff48bfd779cdf6f46217670
Philipp Schilk <schilk.philipp@gmail.com>
2024-02-25 14:35:42 +0100

    NVIM: Add 'update py3 env' function.

--- neovim/.config/nvim/lua/schilk/config/py3_env.lua
+++ neovim/.config/nvim/lua/schilk/config/py3_env.lua
@@ -4,15 +4,16 @@ M.py_env_dir = vim.fs.normalize('~/.config/nvim/nvim_env')
 M.py = vim.fs.normalize(M.py_env_dir .. '/bin/python')
 
 function M.config_py3_env()
-    -- Register command to create py3 venv:
-    vim.cmd("command CreatePy3Env lua require('schilk.config.py3_env').create_py3_env()")
+    -- Register commands:
+    vim.cmd("command Py3EnvCreate lua require('schilk.config.py3_env').create_py3_env()")
+    vim.cmd("command Py3EnvUpdate lua require('schilk.config.py3_env').update_py3_env()")
 
     if (vim.fn.filereadable(M.py) == 1) then
         -- Venv exists: Use it
         vim.g.python3_host_prog = M.py
     else
         -- Venv does not exist. Prompt to create it:
-        vim.print("No NVIM py3 venv exists. Call `:CreatePy3Env`.")
+        vim.print("No NVIM py3 venv exists. Call `:Py3EnvCreate`.")
     end
 end
 
@@ -37,6 +38,24 @@ function M.create_py3_env()
     end
 
     vim.print("NVIM py3 venv created.")
+    vim.g.python3_host_prog = M.py
+end
+
+-- Create a python 3 virtual environment for nvim if one does not
+-- exist yet:
+function M.update_py3_env()
+    if (vim.fn.filereadable(M.py) == 0) then
+        vim.print("NVIM py3 venv does not exist.")
+        return
+    end
+
+    vim.cmd("!" .. M.py .. ' -m pip install --upgrade pynvim 1>/dev/null')
+    if (vim.v.shell_error ~= 0) then
+        vim.print("Unable to update pynvim in venv!")
+        return
+    end
+
+    vim.print("NVIM py3 venv updated.")
     vim.g.python3_host_prog = M.py
 end
 
