42f3e8db20f1a837379c9a05816155252a678b82
Philipp Schilk <schilk.philipp@gmail.com>
2024-04-27 22:08:46 +0200

    nvim: use treesitter highlighting also for markdown.

--- neovim/.config/nvim/lua/schilk/config/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/treesitter.lua
@@ -1,13 +1,20 @@
 local M = {}
 
 function M.config()
+    local ts_highlight_langs = { "hyprlang", "markdown" };
+
     require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "hyprlang" },
+        ensure_installed = { "hyprlang", "markdown" },
 
         highlight = {
             enable = true,
-            disable = function(lang, buf)
-                return lang ~= "hyprlang"
+            disable = function(lang, _) -- (lang, buf)
+                for _, en_lang in pairs(ts_highlight_langs) do
+                    if lang == en_lang then
+                        return false;
+                    end
+                end
+                return true;
             end,
         },
 
