7e9f4092ade7b44fece7d64ed6d66bf5ef5dd67b
Philipp Schilk <schilk.philipp@gmail.com>
2024-06-08 16:34:08 +0200

    nvim: augroup name spellfix.

--- neovim/.config/nvim/lua/schilk/config/plugins/markdown_preview.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/markdown_preview.lua
@@ -4,7 +4,7 @@ function M.config()
     vim.api.nvim_create_autocmd('FileType', {
         desc = 'Markdown preview keybinds',
         pattern = 'markdown',
-        group = vim.api.nvim_create_augroup('md_keybidn', { clear = true }),
+        group = vim.api.nvim_create_augroup('md_keybind', { clear = true }),
         callback = function(opts)
             vim.keymap.set({ 'n' }, '<leader>ll', ':MarkdownPreviewToggle<CR>',
                 { silent = true, desc = "📝 Toggle Markdown Preview", buffer = true })
