b96e8e8e6420e4808ef66fdd9081cbdeb5d7637e
Philipp Schilk <schilk.philipp@gmail.com>
2024-07-11 12:04:14 +0200

    NVIM: Add undotree

--- neovim/.config/nvim/lua/schilk/config/plugins/neogen.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/neogen.lua
@@ -2,8 +2,7 @@ local M = {}
 
 function M.config()
     require('neogen').setup {}
-    vim.keymap.set({ 'n' }, '<leader>mg', require('neogen').generate,
-        { silent = true, desc = "📝 Generate Documentation" })
+    vim.keymap.set({ 'n' }, '<leader>mg', require('neogen').generate, { silent = true, desc = "📝 Generate Documentation" })
 end
 
 return M
