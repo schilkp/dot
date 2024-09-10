1d65440c6b62dea6a0fa491b17c36fb24605ec19
Philipp Schilk <schilk.philipp@gmail.com>
2024-09-10 16:54:20 +0200

    nvim: fixed whichkey icon allignment

--- neovim/.config/nvim/lua/schilk/config/plugins/colorizer.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/colorizer.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
     require('colorizer').setup({})
 
-    vim.keymap.set("n", "<leader>mc", ":ColorizerToggle<CR>", { silent = true, desc = " 🎨 Toggle colorizer." })
+    vim.keymap.set("n", "<leader>mc", ":ColorizerToggle<CR>", { silent = true, desc = "🎨 Toggle colorizer." })
 end
 
 M.spec = {
