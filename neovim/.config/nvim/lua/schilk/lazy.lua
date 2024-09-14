adda7b104c9759e00318ad63043e2bafac2456f5
Philipp Schilk <schilk.philipp@gmail.com>
2024-09-14 19:38:32 +0200

    nvim: use new neovim api for fs_Stat

--- neovim/.config/nvim/lua/schilk/lazy.lua
+++ neovim/.config/nvim/lua/schilk/lazy.lua
@@ -1,24 +1,22 @@
 local M = {}
 
 function M.activate()
-
     -- Install lazy package manager if not already installed
     --    https://github.com/folke/lazy.nvim
     local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-    if not vim.loop.fs_stat(lazypath) then
-      vim.print("Installing lazy.nvim...")
-      vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-      }
-      vim.print("Done!")
+    if not vim.uv.fs_stat(lazypath) then
+        vim.print("Installing lazy.nvim...")
+        vim.fn.system {
+            'git',
+            'clone',
+            '--filter=blob:none',
+            'https://github.com/folke/lazy.nvim.git',
+            '--branch=stable', -- latest stable release
+            lazypath,
+        }
+        vim.print("Done!")
     end
     vim.opt.rtp:prepend(lazypath)
-
 end
 
 return M
