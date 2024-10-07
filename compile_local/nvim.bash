f881cb845129da90f46e1201169d062e4a19f7ba
schilkp <schilk.philipp@gmail.com>
2024-10-07 12:46:54 +0200

    compile_local: update nvim script.

--- compile_local/nvim.bash
+++ compile_local/nvim.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-TARGET="$HOME/.local/nvim"
+TARGET="$HOME/.local/packages/nvim"
 INSTALL_DIR="$HOME/.local/bin"
 JOBS=32
 
 mkdir -p $TARGET
 
+cd $TARGET
+
 git clone https://github.com/neovim/neovim.git
 cd neovim
-# checkout here
 
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$TARGET" -j "$JOBS"
 make install
 
-#TODO: copy
+install ./bin/nvim "$INSTALL_DIR"
