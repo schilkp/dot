2566acd26f6e7b57470ae947c9f6c0a5fd9dd26f
schilkp <schilk.philipp@gmail.com>
2024-12-09 09:16:19 +0100

    emacs: more basic setup

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -7,6 +7,9 @@
 ;; Disable the splash screen
 (setq inhibit-splash-screen t)
 
+;; Disable backup files
+(setq make-backup-files nil)
+
 (load-user-file "schilk/elpaca.el")
 (load-user-file "schilk/evil.el")
 (load-user-file "schilk/fzf.el")
@@ -25,9 +28,31 @@
 
 ;; disable menu & tool bar
 (menu-bar-mode -1)
-(tool-bar-mode -1) 
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 ;; set ui scale in terminal
-(set-face-attribute 'default nil :height 130) 
+(set-face-attribute 'default nil :height 130)
 
 (evil-define-key 'normal 'global (kbd "SPC x") 'org-toggle-checkbox)
+
+(use-package which-key :ensure (:wait t) :demand t)
+(require 'which-key)
+(which-key-mode)
+
+(use-package org-roam :ensure (:wait t) :demand t)
+(use-package org-roam-ui :ensure (:wait t) :demand t)
+
+(setq org-roam-directory (file-truename "~/org-roam"))
+(org-roam-db-autosync-mode)
+
+(use-package helm :ensure (:wait t) :demand t)
+
+(setq org-roam-capture-templates
+      '(("d" "default" plain
+         "%?"
+         :if-new (file+head "${slug}.org" 
+                            "#+title: ${title}\n")
+         :unnarrowed t)))
+
+(use-package consult :ensure (:wait t) :demand t)
