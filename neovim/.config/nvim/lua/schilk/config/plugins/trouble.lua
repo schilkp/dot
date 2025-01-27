f629922a0239ca588ed02b448c5c3e5ff175dedb
schilkp <schilk.philipp@gmail.com>
2025-01-27 07:02:04 +0100

    nvim: update trouble config to enable lazy loading

--- neovim/.config/nvim/lua/schilk/config/plugins/trouble.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/trouble.lua
@@ -3,7 +3,15 @@ local M = {}
 M.spec = {
     'folke/trouble.nvim',
     opts = {},
+    lazy = true,
     cond = not vim.g.vscode, -- Disable in vscode-neovim
+    keys = {
+        {
+            "<leader>gH",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "LSP: Open Diagnostics Pane"
+        }
+    }
 }
 
 return M
