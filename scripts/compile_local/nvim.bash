710a20fef9ed2637f47643b83fb087f64eb7ee4b
Philipp Schilk <schilk.philipp@gmail.com>
2025-04-23 13:44:43 +0200

    SCRIPTS: Improve nvim compile script

--- scripts/compile_local/nvim.bash
+++ scripts/compile_local/nvim.bash
@@ -4,15 +4,22 @@ set -e -x
 TARGET="$HOME/.local/packages/nvim"
 INSTALL_DIR="$HOME/.local/bin"
 JOBS=32
+VERSION="v0.11.0"
 
 mkdir -p "$TARGET"
-
 cd "$TARGET"
 
-git clone https://github.com/neovim/neovim.git
-cd neovim
+if [ ! -d "$TARGET/neovim" ]; then
+    echo "Cloning Neovim repository..."
+    git clone https://github.com/neovim/neovim.git "$TARGET/neovim"
+else
+    echo "Neovim repository already exists at $TARGET/neovim"
+fi
+
+cd "$TARGET/neovim"
+git checkout $VERSION
 
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$TARGET" -j "$JOBS"
 make install
 
-install ./bin/nvim "$INSTALL_DIR"
+install "$TARGET"/neovim/bin/nvim "$INSTALL_DIR"
