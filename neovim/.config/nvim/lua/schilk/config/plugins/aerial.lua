f2bee159257d627912a03fe89168e7a131b54987
schilkp <schilk.philipp@gmail.com>
2025-05-01 12:12:01 +0200

    nvim: support verilog/sverilog in aerial with custom fork (for now)

--- neovim/.config/nvim/lua/schilk/config/plugins/aerial.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/aerial.lua
@@ -1,17 +1,40 @@
 local M = {}
 
+local ALL_KINDS = vim.tbl_filter(
+    function(k) return type(k) == "string" end,
+    vim.tbl_keys(vim.lsp.protocol.SymbolKind)
+)
+
 function M.config()
     require("aerial").setup({
-        backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+        backends = {
+            ['_'] = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+            verilog = { "treesitter" },
+            systemverilog = { "treesitter" },
+        },
         layout = {
             min_width = 15,
+        },
+        filter_kind = {
+            ['_'] = {
+                "Class",
+                "Constructor",
+                "Enum",
+                "Function",
+                "Interface",
+                "Module",
+                "Method",
+                "Struct",
+            },
+            verilog = ALL_KINDS,
+            systemverilog = ALL_KINDS
         }
     })
     vim.keymap.set({ 'n' }, '<leader>mo', '<cmd>AerialToggle!<CR>', { silent = true, desc = "📋 Toggle Outline." }) -- TODO make saga?
 end
 
 M.spec = {
-    "stevearc/aerial.nvim",
+    "schilkp/aerial.nvim",
     config = M.config,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
