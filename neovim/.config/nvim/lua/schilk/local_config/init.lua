6e868ad60fb037f48510d139cfed7711f641ecbe
schilkp <schilk.philipp@gmail.com>
2025-05-02 15:57:42 +0200

    nvim: use `:trust` instead of re-implementing :)

--- neovim/.config/nvim/lua/schilk/local_config/init.lua
+++ neovim/.config/nvim/lua/schilk/local_config/init.lua
@@ -1,131 +1,16 @@
---[[
-
-NOTE: Originally from `nvim-config-local` by klen, which is released under the
-MIT License
-
-https://github.com/klen/nvim-config-local
-
-I inlined this project both because I wanted to make some modifications and
-because I wanted it to always be available - even before Lazy loads. This way
-I can also inject local lazy plugins.
-
----
-
-MIT License
-
-Copyright (c) 2021 klen
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-]]
-
 local M = {}
-local HashMap = require "schilk.local_config.hashmap"
-
---//===--------------------------------------------------------------------===//
---// State
---//===--------------------------------------------------------------------===//
-
-local hashmap
 
 --//===--------------------------------------------------------------------===//
 --// Config Constants
 --//===--------------------------------------------------------------------===//
 
 M.CONFIG_FILE_NAMES = { ".schilk.nvim.lua" };
-M.HASH_FILE = vim.fs.joinpath(vim.fn.stdpath("data"), "trusted_local_configs");
 M.CONFIG_SAMPLES_DIR = vim.fs.joinpath(vim.fn.stdpath("config"), "example_local_configs");
 
 --//===--------------------------------------------------------------------===//
---// Utils
+--// Commands
 --//===--------------------------------------------------------------------===//
 
-local function prompt_choice(msg, choices)
-    local _, choice = pcall(vim.fn.confirm, "[local_config]: " .. msg, choices, 1)
-    return choice
-end
-
-local function map(t, f)
-    local t1 = {}
-    local t_len = #t
-    for i = 1, t_len do
-        t1[i] = f(t[i])
-    end
-    return t1
-end
-
-local function info(msg)
-    vim.notify("[local_config]: " .. msg, vim.log.levels.INFO)
-end
-
-local function warn(msg)
-    vim.notify("[local_config]: " .. msg, vim.log.levels.WARN)
-end
-
-local function error(msg)
-    vim.notify("[local_config]: " .. msg, vim.log.levels.ERROR)
-end
-
---//===--------------------------------------------------------------------===//
---// Commands: Mark trusted/untrusted
---//===--------------------------------------------------------------------===//
-
----Deny local configuration
----@param filename? string: a file name
-function M.deny(filename)
-    filename = filename or M.lookup()
-    if not filename then
-        return error("Config file not found: " .. table.concat(M.CONFIG_FILE_NAMES, ","))
-    end
-    hashmap:write(filename, "!")
-    info('Config file "' .. filename .. '" marked as denied')
-end
-
----Mark the given filename as trusted
---- @param filename string: a file name
-function M.trust(filename)
-    hashmap:trust(filename)
-    info('Config file "' .. filename .. '" marked as trusted.')
-end
-
---//===--------------------------------------------------------------------===//
---// Commands: Edit
---//===--------------------------------------------------------------------===//
-
----Confirm a config file on save
-function M.confirm()
-    local filename = vim.fn.expand("%:p")
-    local state = hashmap:verify(filename)
-    if state ~= "t" then
-        local choice = prompt_choice(
-            "Do you want to mark this config as trusted: " .. filename .. "?",
-            "&Yes\n&No"
-        )
-        if choice == 1 then
-            return M.trust(filename)
-        else
-            return M.deny(filename)
-        end
-    end
-end
-
----Confirm a config file on save
 function M.copy_to_samples()
     local filename = vim.fn.expand("%:p")
 
@@ -160,11 +45,6 @@ function M.edit(filename)
     vim.api.nvim_command("edit " .. filename)
 end
 
----Edit the hash file
-function M.edit_hash_file()
-    vim.api.nvim_command("edit " .. M.HASH_FILE)
-end
-
 ---Look for config file
 function M.lookup()
     for _, filename in ipairs(M.CONFIG_FILE_NAMES) do
@@ -175,73 +55,28 @@ function M.lookup()
     end
 end
 
----Look for a config if it exist in the current directory, and verify if it is trusted.
----If not, prompt the user on what they want to do.
-function M.find_trusted_local_config()
-    local filename = M.lookup()
-    if filename then
-        local trust_state = hashmap:verify(filename)
-
-        if trust_state == "t" then
-            info('Trusted local config file "' .. filename .. '" found.')
-            return filename
-        elseif trust_state == "u" then
-            -- Prompt
-            local msg = 'Unknown config file found: "' .. vim.fn.fnamemodify(filename, ":~:.") .. '"'
-            local choice = prompt_choice(msg, "&ignore\n&view\n&deny\n&allow")
-
-            if choice == 2 then
-                M.edit(filename)
-                return ""
-            elseif choice == 3 then
-                M.deny(filename)
-                return ""
-            elseif choice == 4 then
-                M.trust(filename)
-                return filename
-            end
-        else
-            -- Config denied.
-            warn('Local config file "' .. filename .. '" is denied and has not been loaded.')
-            return ""
-        end
-    end
-    return ""
-end
-
+--Look for config file & source if trusted
 function M.source()
-    local filename = M.find_trusted_local_config();
-    if not filename or filename == "" then
+    local file = M.lookup();
+    if not file or file == "" then
         return
     end
-    vim.api.nvim_command("source " .. filename)
+    local content = vim.secure.read(file)
+    if content ~= nil then
+        vim.api.nvim_command("source " .. file)
+        vim.defer_fn(function()
+            vim.notify("[local_config]: loaded local config file.", vim.log.levels.INFO)
+        end, 250)
+    else
+        vim.defer_fn(function()
+            vim.notify("[local_config]: local config file found but not trusted!", vim.log.levels.WARN)
+        end, 250)
+    end
 end
 
 function M.setup()
-    hashmap = HashMap:init(M.HASH_FILE)
-
     vim.api.nvim_command "command! LocalEdit lua require'schilk.local_config'.edit()<CR>"
-    vim.api.nvim_command "command! LocalEditDB lua require'schilk.local_config'.edit_hash_file()<CR>"
-    vim.api.nvim_command "command! LocalTrust lua require'schilk.local_config'.trust(vim.fn.expand('%:p'))<CR>"
-    vim.api.nvim_command "command! LocalDeny lua require'schilk.local_config'.deny()<CR>"
     vim.api.nvim_command "command! LocalSaveAsSample lua require'schilk.local_config'.copy_to_samples()<CR>"
-
-    -- local au = vim.api.nvim_create_autocmd
-    -- local augroup = vim.api.nvim_create_augroup("local-config", { clear = true })
-
-    -- Confirm local configs
-    -- au("BufWritePost", {
-    --     group = augroup,
-    --     desc = "Confirm local configs",
-    --     pattern = table.concat(
-    --         map(M.CONFIG_FILE_NAMES, function(f)
-    --             return "**/" .. f
-    --         end),
-    --         ","
-    --     ),
-    --     nested = true,
-    --     callback = M.confirm,
-    -- })
 end
 
 return M
