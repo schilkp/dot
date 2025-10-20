6d8ad7e5489c8368d1d60e1f66d26a8cbf1c08c3
Philipp Schilk <schilk.philipp@gmail.com>
2025-10-20 11:19:02 +0200

    nvim: add systemverilog flip-port macro

--- neovim/.config/nvim/lua/schilk/utils/macros/init.lua
+++ neovim/.config/nvim/lua/schilk/utils/macros/init.lua
@@ -3,6 +3,8 @@ local M = {}
 function M.setup()
 	local sv_modules = require("schilk.utils.macros.sv.module_instantiation")
 	sv_modules.setup()
+	local sv_flip = require("schilk.utils.macros.sv.flip_ports")
+	sv_flip.setup()
 end
 
 return M
---
+++ neovim/.config/nvim/lua/schilk/utils/macros/sv/flip_ports.lua
@@ -0,0 +1,40 @@
+local M = {}
+
+local utils = require("schilk.utils.macros.utils")
+
+function M.flip_input_output(lines)
+	local result = {}
+
+	for _, line in ipairs(lines) do
+		local modified_line = line
+
+		-- Check if line contains input or output (ignoring leading whitespace)
+		local trimmed = line:match("^%s*(.-)%s*$")
+
+		if trimmed:match("^input%s") then
+			-- Replace input with output
+			modified_line = line:gsub("(%s*)input(%s)", "%1output%2")
+			modified_line = modified_line:gsub("_i(%s*[,%s])", "_o%1")
+			modified_line = modified_line:gsub("_i$", "_o")
+			modified_line = modified_line:gsub("_ni(%s*[,%s])", "_no%1")
+			modified_line = modified_line:gsub("_ni$", "_no")
+		elseif trimmed:match("^output%s") then
+			-- Replace output with input
+			modified_line = line:gsub("(%s*)output(%s)", "%1input%2")
+			modified_line = modified_line:gsub("_o(%s*[,%s])", "_i%1")
+			modified_line = modified_line:gsub("_o$", "_i")
+			modified_line = modified_line:gsub("_no(%s*[,%s])", "_ni%1")
+			modified_line = modified_line:gsub("_no$", "_ni")
+		end
+
+		table.insert(result, modified_line)
+	end
+
+	return result
+end
+
+function M.setup()
+	vim.keymap.set("v", "gF", utils.visual_process_selection(M.flip_input_output))
+end
+
+return M
