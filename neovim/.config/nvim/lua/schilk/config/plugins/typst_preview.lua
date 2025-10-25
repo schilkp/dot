bd7e3749aa7772bfa1ca82b6f07910f9bb671beb
schilkp <schilk.philipp@gmail.com>
2025-10-25 14:50:53 +0200

    nvim: use fixed port for typst preview.

--- neovim/.config/nvim/lua/schilk/config/plugins/typst_preview.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/typst_preview.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 function M.config()
-    require("typst-preview").setup({})
+    require("typst-preview").setup({
+        port = 9002
+    })
     vim.keymap.set({ 'n' }, '<leader>ll', ":TypstPreviewToggle<cr>", { silent = true, desc = "Typst: Preview Toggle." })
     vim.keymap.set({ 'n' }, '<leader>lf', ":TypstPreviewFollowCursorToggle<cr>", { silent = true, desc = "Typst: Follow Cursor Toggle." })
     vim.keymap.set({ 'n' }, '<leader>lv', ":TypstPreviewSyncCursor<cr>", { silent = true, desc = "Typst: Sync Cursor." })
