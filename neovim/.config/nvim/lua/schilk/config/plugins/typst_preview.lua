f7d8865afddc66f7d61e856a149f3a106f99b504
schilkp <schilk.philipp@gmail.com>
2025-11-09 08:45:02 +0100

    nvim: enable changing of typst preview port on the fly
    useful for when used via port forwarding, but otherwise a random port
    is more conveniant as multiple tabs don't interfeer.

--- neovim/.config/nvim/lua/schilk/config/plugins/typst_preview.lua
+++ neovim/.config/nvim/lua/schilk/config/plugins/typst_preview.lua
@@ -14,6 +14,31 @@ function M.config()
     { silent = true, desc = "Typst: Follow Cursor Toggle." }
   )
   vim.keymap.set({ "n" }, "<leader>lv", ":TypstPreviewSyncCursor<cr>", { silent = true, desc = "Typst: Sync Cursor." })
+
+  -- Function to set port:
+  vim.api.nvim_create_user_command("TypstPreviewSetPort", function(opts)
+    local port
+    if #opts.fargs == 0 then
+      -- Default to random port:
+      port = 0
+    else
+      port = tonumber(opts.fargs[1])
+      if not port then
+        vim.notify("Invalid port number", vim.log.levels.ERROR)
+        return
+      end
+    end
+    require("typst-preview.config").opts.port = port
+
+    local port_str = tostring(port)
+    if port == 0 then
+      port_str = "random"
+    end
+    vim.notify("Typst preview port set to " .. port_str .. " (will apply to next server started)", vim.log.levels.INFO)
+  end, {
+    nargs = "?",
+    desc = "Set the port for the next typst preview server",
+  })
 end
 
 ---@type LazyPluginSpec
