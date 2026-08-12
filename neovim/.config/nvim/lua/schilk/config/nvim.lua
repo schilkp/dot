1b94be3a44d6a4498ded5b5fe2a45c0c7d105871
schilkp <schilk.philipp@gmail.com>
2026-08-12 13:21:11 +0200

    nvim: don't use deprecated `vim.highlight`.

--- neovim/.config/nvim/lua/schilk/config/nvim.lua
+++ neovim/.config/nvim/lua/schilk/config/nvim.lua
@@ -8,7 +8,7 @@ function M.config_highlight_on_yank()
   local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
   vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-      vim.highlight.on_yank({ timeout = 40 })
+      vim.hl.on_yank({ timeout = 40 })
     end,
     group = highlight_group,
     pattern = "*",
