fd5e58fefbe4ac8fb9d2b2800f0c88ba623ed4e2
schilkp <schilk.philipp@gmail.com>
2026-09-17 07:41:46 +0200

    nvim: simplify lua macro util funcs

--- neovim/.config/nvim/lua/schilk/utils/macros/utils.lua
+++ neovim/.config/nvim/lua/schilk/utils/macros/utils.lua
@@ -2,20 +2,18 @@ local M = {}
 
 function M.visual_process_selection(processing_func)
   return function()
-    -- Determine range of lines selected:
-    local line_first = vim.fn.line("v")
-    local line_last = vim.fn.line(".")
-    if line_first > line_last then
-      line_first, line_last = line_last, line_first
-    end
-
-    local bufn = vim.api.nvim_get_current_buf()
-
     -- Retrieve selected lines:
-    local lines = vim.api.nvim_buf_get_lines(bufn, line_first - 1, line_last, false)
+    local pos1 = vim.fn.getpos("v")
+    local pos2 = vim.fn.getpos(".")
+    local lines = vim.fn.getregion(pos1, pos2, { type = "V" })
 
     -- Process selected lines using the provided function:
     local processed_lines = processing_func(lines)
+
+    -- Determine buffer and (ordered) line range selected for replacement:
+    local bufn = vim.api.nvim_get_current_buf()
+    local line_first = math.min(pos1[2], pos2[2])
+    local line_last = math.max(pos1[2], pos2[2])
 
     -- Replace selected lines:
     vim.api.nvim_buf_set_lines(bufn, line_first - 1, line_last, false, processed_lines)
@@ -32,10 +30,8 @@ function M.cmd_process_selection(processing_func)
     local line_start = opts.line1
     local line_end = opts.line2
 
-    -- Get current buffer
-    local bufn = vim.api.nvim_get_current_buf()
-
     -- Retrieve selected lines (nvim_buf_get_lines uses 0-indexed, exclusive end)
+    local bufn = vim.api.nvim_get_current_buf()
     local lines = vim.api.nvim_buf_get_lines(bufn, line_start - 1, line_end, false)
 
     -- Process selected lines using the provided function
